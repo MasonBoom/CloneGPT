80ddcfe1cae0a33cacf8e89a28225c518907c7ec
MasonBoom <masonboom@gmail.com>
2023-06-07 15:05:13 -0400

    started homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 function HomePage() {
+
   return (
-    <div>
-      <h1>CloneGPT</h1>
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      <h1 className="text-5xl font-bold mb-20">CloneGPT</h1>
+      <div>
+        <h2>Examples</h2>
+      </div>
     </div>
   )
 }
